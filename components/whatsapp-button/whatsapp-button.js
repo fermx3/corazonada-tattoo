14e1e41b559fad2c0970aad94e0eccf3dbff76df
Fernando Rios Alcantara <fermx3@gmail.com>
2024-07-05 00:01:41 -0600

    Add WhatsApp button link

--- components/whatsapp-button/whatsapp-button.js
+++ components/whatsapp-button/whatsapp-button.js
@@ -1,9 +1,17 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function WhatsappButton() {
   return (
     <div className='fixed bottom-5 right-5'>
-      <Image src='/icons/whatsapp.svg' alt='Whatsapp' width={50} height={50} />
+      <Link href='https://wa.me/529841447501' target='_blank'>
+        <Image
+          src='/icons/whatsapp.svg'
+          alt='Whatsapp'
+          width={50}
+          height={50}
+        />
+      </Link>
     </div>
   );
 }
