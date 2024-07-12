3a64dbfb9f0d431f1df8261e9c58367a862d2f85
Fernando Rios Alcantara <fermx3@gmail.com>
2024-07-11 21:45:10 -0600

    chore: Improve TatuajesGrid component swipe functionality

--- components/tatuadores-invitados-section/tatuajes-grid.js
+++ components/tatuadores-invitados-section/tatuajes-grid.js
@@ -7,6 +7,28 @@ import { useState } from 'react';
 export default function TatuajesGrid({ tatuajes, slug }) {
   const [modalOpenIndex, setModalOpenIndex] = useState(null);
 
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe)
+      isLeftSwipe ? handleNext() : isRightSwipe && handlePrev();
+  };
+
   const handleClick = (index) => {
     setModalOpenIndex(index);
   };
@@ -47,6 +69,9 @@ export default function TatuajesGrid({ tatuajes, slug }) {
               alt=''
               fill
               className='object-cover rounded-lg'
+              onTouchStart={onTouchStart}
+              onTouchMove={onTouchMove}
+              onTouchEnd={onTouchEnd}
             />
           </div>
           {modalOpenIndex > 0 && (
