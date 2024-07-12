1ea8f81beff21b4dbc436011cd357ad08e9c616d
Fernando Rios Alcantara <fermx3@gmail.com>
2024-07-12 13:22:34 -0600

    Refactor WhatsappButton component to use framer-motion for animation

--- components/whatsapp-button/whatsapp-button.js
+++ components/whatsapp-button/whatsapp-button.js
@@ -1,9 +1,29 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { motion } from 'framer-motion';
+
 export default function WhatsappButton() {
   return (
-    <div className='fixed bottom-5 right-5'>
+    <motion.div
+      className='fixed bottom-5 right-5'
+      initial={{ opacity: 0, scale: 0, y: 20 }}
+      animate={{ opacity: 1, scale: 1, y: 0 }}
+      transition={{
+        ease: 'easeIn',
+        duration: 0.1,
+        type: 'spring',
+        mass: 0.5,
+      }}
+      whileTap={{
+        scale: 0.9,
+      }}
+      whileHover={{
+        scale: 1.05,
+      }}
+    >
       <Link href='https://wa.me/529841447501' target='_blank'>
         <Image
           src='/icons/whatsapp.svg'
@@ -12,6 +32,6 @@ export default function WhatsappButton() {
           height={50}
         />
       </Link>
-    </div>
+    </motion.div>
   );
 }
